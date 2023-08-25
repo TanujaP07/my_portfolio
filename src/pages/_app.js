4430e8f286f0f66cf351bafab7b8f25905dbf1f7
TanujaP07 <tanuja.pyneni7898@gmail.com>
2023-08-25 13:01:00 -0500

    portfolio updates

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,18 @@
 import '@/styles/globals.css';
 import Navbar from 'components/Navbar';
-import { Inter } from '@next/font/google'
+// import { Inter } from "@next/font/google"
  
 // If loading a variable font, you don't need to specify the font weight
-const inter = Inter({ subsets: ['latin'] })
-
+// const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
   return (
-    <main className={inter.className}>
+    // <main className={inter.className}>
+    <>
     <Navbar/>
   <Component {...pageProps} />
-  </main>
+  </>
+  // </main>
   )
 }
 
